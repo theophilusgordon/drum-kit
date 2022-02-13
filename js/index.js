4b03bef200c1079902d942340d88d71eaf51bedc
Theophilus Gordon <86818074+theophilusgordon@users.noreply.github.com>
2022-02-13 13:12:45 +0000

    Uploads audio files to main

--- js/index.js
+++ js/index.js
@@ -14,13 +14,13 @@ document.addEventListener('keypress', event => {
 });
 
 function makeSound(key){
-  if(key === 'w') new Audio('../sounds/tom-1.mp3').play();
-  if(key === 'a') new Audio('../sounds/tom-2.mp3').play();
-  if(key === 's') new Audio('../sounds/tom-3.mp3').play();
-  if(key === 'd') new Audio('../sounds/tom-4.mp3').play();
-  if(key === 'j') new Audio('../sounds/snare.mp3').play();
-  if(key === 'k') new Audio('../sounds/kick-bass.mp3').play();
-  if(key === 'l') new Audio('../sounds/crash.mp3').play();
+  if(key === 'w') new Audio('sounds/tom-1.mp3').play();
+  if(key === 'a') new Audio('sounds/tom-2.mp3').play();
+  if(key === 's') new Audio('sounds/tom-3.mp3').play();
+  if(key === 'd') new Audio('sounds/tom-4.mp3').play();
+  if(key === 'j') new Audio('sounds/snare.mp3').play();
+  if(key === 'k') new Audio('sounds/kick-bass.mp3').play();
+  if(key === 'l') new Audio('sounds/crash.mp3').play();
 }
 
 function drumAnimation(currentKey){
